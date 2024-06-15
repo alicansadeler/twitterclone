2496db7f3c0e51589f5f2a97ba73e5cd3d6a7166
alicansadeler <alicansadeler@gmail.com>
2024-06-15 15:19:32 +0300

    sidebar logolar ve button eklendi

--- src/COMPONENTS/HomeComponents/Sidebar.jsx
+++ src/COMPONENTS/HomeComponents/Sidebar.jsx
@@ -1,3 +1,56 @@
+import XLOGO from "../../assets/sidebarLogo/icons8-twitterx-30.png";
+import HomeLogo from "../../assets/sidebarLogo/home.png";
+import ExploreLogo from "../../assets/sidebarLogo/explore.png";
+import NotificationsLogo from "../../assets/sidebarLogo/Notifications.png";
+import MessagesLogo from "../../assets/sidebarLogo/Messages.png";
+import BookmarksLogo from "../../assets/sidebarLogo/Bookmarks.png";
+import ListsLogo from "../../assets/sidebarLogo/Lists.png";
+import ProfileLogo from "../../assets/sidebarLogo/Profile.png";
+import More from "../../assets/sidebarLogo/More.png";
+import TweetButton from "../../assets/sidebarLogo/Tweet-Buton-Xl.png";
+
 export const Sidebar = () => {
-  return <h1>Sidebar Page</h1>;
+  return (
+    <>
+      <div className="sidebar-container">
+        <div className="sidebar-item">
+          <img src={XLOGO} alt="XLOGO" />
+        </div>
+        <div className="sidebar-item">
+          <img src={HomeLogo} alt="Bookmarks" />
+          <span>Bookmarks</span>
+        </div>
+        <div className="sidebar-item">
+          <img src={ExploreLogo} alt="ExploreLogo" />
+          <span>Explore</span>
+        </div>
+        <div className="sidebar-item">
+          <img src={NotificationsLogo} alt="NotificationsLogo" />
+          <span>Notifications</span>
+        </div>
+        <div className="sidebar-item">
+          <img src={MessagesLogo} alt="MessagesLogo" />
+          <span>Messages</span>
+        </div>
+        <div className="sidebar-item">
+          <img src={BookmarksLogo} alt="BookmarksLogo" />
+          <span>Bookmarks</span>
+        </div>
+        <div className="sidebar-item">
+          <img src={ListsLogo} alt="ListsLogo" />
+          <span>Lists</span>
+        </div>
+        <div className="sidebar-item">
+          <img src={ProfileLogo} alt="ProfileLogo" />
+          <span>Profile</span>
+        </div>
+        <div className="sidebar-item">
+          <img src={More} alt="More" />
+          <span>More</span>
+        </div>
+
+        <button className="sidebar-button">Twiit</button>
+      </div>
+    </>
+  );
 };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,20 @@
+import { Sidebar } from "../COMPONENTS/HomeComponents/Sidebar";
+import { TimeLine } from "../COMPONENTS/HomeComponents/TimeLine";
+import { TrendList } from "../COMPONENTS/HomeComponents/TrendList";
 export const Home = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <div className="homepage-container">
+        <section className="homepage-sidebar">
+          <Sidebar />
+        </section>
+        <section className="homepage-timeline">
+          <TimeLine />
+        </section>
+        <section className="homepage-trend">
+          <TrendList />
+        </section>
+      </div>
+    </>
+  );
 };
