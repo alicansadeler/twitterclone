0bbe58d34549751d388f8b7b47c983a65e9e1d43
alicansadeler <alicansadeler@gmail.com>
2024-06-17 10:44:40 +0300

    trendlist için sahte veri oluşturuldu

---
+++ src/dataTrend.js
@@ -0,0 +1,29 @@
+const dataTrend = [
+  {
+    name: "#TechNews",
+    country: "Worldwide",
+    tweet_volume: 12000,
+  },
+  {
+    name: "#GamingCommunity",
+    country: "United States",
+    tweet_volume: 8500,
+  },
+  {
+    name: "#SummerVibes",
+    country: "Brazil",
+    tweet_volume: 6500,
+  },
+  {
+    name: "#WorldEnvironmentDay",
+    country: "India",
+    tweet_volume: 18000,
+  },
+  {
+    name: "#SpaceExploration",
+    country: "United Kingdom",
+    tweet_volume: 2200,
+  },
+];
+
+export default dataTrend;
